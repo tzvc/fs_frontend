6914f13f7cee27de7c5160dbe95386fbff01f9e1
theochampion <contact.theochampion@gmail.com>
2019-09-30 21:11:53 +0200

    :construction: Added state to signup page

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -34,27 +34,60 @@ const Title = styled.h1`
 	}
 `;
 
-const SignUpPage = () => (
-	<Content>
-		<Title>Sign Up</Title>
-		<Spacer />
-		<Form>
-			<label for="male">Username</label>
-			<input placeholder="Enter a username" />
-			<label for="male">Password</label>
-			<input placeholder="Enter your password" />
-			<label for="male">Confirmation</label>
-			<input placeholder="Password confirmation" />
-		</Form>
-		{/* <Spacer>OR</Spacer> */}
-		<Spacer />
+export default class SignUpPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { username: "", password: "", password_conf: "" };
+	}
 
-		<Button>Sign up</Button>
-		<Spacer>
-			Already have an account?&nbsp;
-			<StyledLink to={SIGNIN_ROUTE}>Sign in</StyledLink>
-		</Spacer>
-	</Content>
-);
+	_handleFormChange = event => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
 
-export default SignUpPage;
+	_handleFormSubmit = event => {
+		event.preventDefault();
+		// TODO(theo) send payload
+	};
+
+	render() {
+		return (
+			<Content>
+				<Title>Sign Up</Title>
+				<Spacer />
+				<Form onSubmit={this._handleFormSubmit}>
+					<label htmlFor="username">Username</label>
+					<input
+						name="username"
+						type="text"
+						placeholder="Enter a username"
+						value={this.state.username}
+						onChange={this._handleFormChange}
+					/>
+					<label>Password</label>
+					<input
+						name="password"
+						type="password"
+						placeholder="Enter your password"
+						value={this.state.password}
+						onChange={this._handleFormChange}
+					/>
+					<label>Confirmation</label>
+					<input
+						name="password_conf"
+						type="password"
+						placeholder="Password confirmation"
+						value={this.state.password_conf}
+						onChange={this._handleFormChange}
+					/>
+					<Button>Sign up</Button>
+				</Form>
+				<Spacer />
+
+				<Spacer>
+					Already have an account?&nbsp;
+					<StyledLink to={SIGNIN_ROUTE}>Sign in</StyledLink>
+				</Spacer>
+			</Content>
+		);
+	}
+}
