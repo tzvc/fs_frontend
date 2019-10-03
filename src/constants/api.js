229e8232533206fb371542855c70ecc3dd7c7403
theochampion <contact.theochampion@gmail.com>
2019-10-03 22:42:53 +0200

    :wrench: Config default port 80

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
 export const API_ROOT =
 	process.env.NODE_ENV === "production"
-		? "http://localhost:3000/api"
-		: "https://upbeat-yonath-d53669.netlify.com:3000/api";
+		? "http://localhost/api"
+		: "https://upbeat-yonath-d53669.netlify.com/api";
